971b06ceae8e83ddd2d8b535744b2e6b3d03a375
HAJIME Fukuna <f97one@gmail.com>
2019-05-01 00:08:50 +0000

    APIアクセスのGETに失敗した場合の処理を追加。

---
+++ TaskMeasure/Repositories/ApiAccessException.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Net
+Imports RestSharp
+
+Namespace Repositories
+    Public Class ApiAccessException
+        Inherits Exception
+
+        Private _StatusCode As HttpStatusCode
+        Private _ResponseStatus As ResponseStatus
+
+        Public Property StatusCode() As HttpStatusCode
+            Get
+                Return _StatusCode
+            End Get
+            Private Set
+                _StatusCode = Value
+            End Set
+        End Property
+
+        Public Property ResponseStatus() As ResponseStatus
+            Get
+                Return _ResponseStatus
+            End Get
+            Private Set
+                _ResponseStatus = Value
+            End Set
+        End Property
+
+        Public Sub New()
+            MyBase.New()
+        End Sub
+
+        Public Sub New(statusCode As HttpStatusCode)
+            MyBase.New()
+            _statusCode = statusCode
+        End Sub
+
+        Public Sub New(statusCode As HttpStatusCode, responseStatus As ResponseStatus)
+            MyBase.New()
+            _StatusCode = statusCode
+            _ResponseStatus = responseStatus
+        End Sub
+
+    End Class
+End Namespace
--- TaskMeasure/Repositories/RedmineRepository.vb
+++ TaskMeasure/Repositories/RedmineRepository.vb
@@ -7,22 +7,42 @@ Namespace Repositories
     Public Class RedmineRepository
 
         Public Function GetProjects() As List(Of RedmineProject)
-            Dim content As String = GetContentFromApi("projects", Method.GET, New Dictionary(Of String, String))
+            Dim params As New Dictionary(Of String, String)
+            Dim resp As RestResponse = ExecuteRequest("projects", Method.GET, params)
 
-            Dim proj = JsonConvert.DeserializeObject(Of ProjectsEntity)(content)
+            If resp.ResponseStatus = ResponseStatus.Completed Then
+                Dim proj = JsonConvert.DeserializeObject(Of ProjectsEntity)(resp.Content)
 
-            Return proj.Projects
+                Return proj.Projects
+            Else
+                Throw New ApiAccessException(resp.StatusCode, resp.ResponseStatus)
+            End If
         End Function
 
         Public Function GetIssues(project As RedmineProject) As List(Of RedmineIssue)
             Dim params As New Dictionary(Of String, String)
             params.Add("project_id", project.Id.ToString())
+            Dim resp As RestResponse = ExecuteRequest("issues", Method.GET, params)
 
-            Dim content As String = GetContentFromApi("issues", Method.GET, params)
+            If resp.ResponseStatus = ResponseStatus.Completed Then
+                Dim issues = JsonConvert.DeserializeObject(Of IssuesEntity)(resp.Content)
+                Return issues.Issues
+            Else
+                Throw New ApiAccessException(resp.StatusCode, resp.ResponseStatus)
+            End If
+        End Function
 
-            Dim issues = JsonConvert.DeserializeObject(Of IssuesEntity)(content)
+        Public Function CreateTimeEntry(issue As RedmineIssue, elapsedTime As Double) As RedmineResponseEntity
+            Dim timeEntry As TimeEntry = New TimeEntry(issue, elapsedTime)
+            Dim param = JsonConvert.SerializeObject(timeEntry)
 
-            Return issues.Issues
+            Dim clt As RestClient = GetClient("time_entry")
+            Dim req As RestRequest = GetRequest(Method.POST)
+            req.AddParameter("time_entry", param)
+
+            Dim response As RestResponse = clt.Execute(req)
+
+            Return New RedmineResponseEntity(response.StatusCode, response.Content)
         End Function
 
         Private Function GetClient(endpoint As String) As RestClient
@@ -41,7 +61,7 @@ Namespace Repositories
             Return req
         End Function
 
-        Private Function GetContentFromApi(endpoint As String, method As Method, params As IDictionary(Of String, String)) As String
+        Private Function ExecuteRequest(endpoint As String, method As Method, params As IDictionary(Of String, String)) As RestResponse
             Dim clt As RestClient = GetClient(endpoint)
             Dim req As RestRequest = GetRequest(method)
 
@@ -49,9 +69,7 @@ Namespace Repositories
                 req.AddParameter(kvp.Key, kvp.Value, ParameterType.GetOrPost)
             Next
 
-            Dim response As RestResponse = clt.Execute(req)
-
-            Return response.Content
+            Return clt.Execute(req)
         End Function
 
     End Class
