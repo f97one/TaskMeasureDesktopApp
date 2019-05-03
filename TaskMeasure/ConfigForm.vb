2d4fea10104787459d8311dc73dbd4f36819c254
HAJIME Fukuna <f97one@gmail.com>
2019-05-03 02:27:07 +0000

    設定がないときに設定を書くまでウィンドウを閉じられないような処理を追加。

--- TaskMeasure/ConfigForm.vb
+++ TaskMeasure/ConfigForm.vb
@@ -27,4 +27,23 @@
         Close()
     End Sub
 
+    Private Sub ConfigForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Dim redmineUrl = My.Settings.RedmineUrl
+        Dim apikey = My.Settings.UserApiKey
+
+        If (String.IsNullOrWhiteSpace(redmineUrl)) Then
+            MessageBox.Show("Redmine URL を設定してください。")
+            ToolStripStatusLabel1.Text = "Redmine URL は必須です"
+            e.Cancel = True
+        ElseIf String.IsNullOrWhiteSpace(apikey) Then
+            MessageBox.Show("ユーザーの API アクセスキーを設定してください。")
+            ToolStripStatusLabel1.Text = "API Keyは必須です"
+            e.Cancel = True
+        ElseIf Not (redmineUrl.StartsWith("http://")) And Not (redmineUrl.StartsWith("https://")) Then
+            MessageBox.Show("Redmine URL を設定してください。")
+            ToolStripStatusLabel1.Text = "URLがhttp://, またはhttp://で始まっていません"
+            e.Cancel = True
+        End If
+
+    End Sub
 End Class
